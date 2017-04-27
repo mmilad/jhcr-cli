db12913329f274a1514a7e23df3b9f70a30a7420
Meister <Meister>
2017-04-28 00:42:13 +0200

    updated

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 #! /usr/bin/env node
 var exec = require('child_process').exec;
-var userArgs = process.argv.slice(2);
 var fs = require('fs');
-var pref="test/";
+var path = require('path');
+var jhcrPath = process.argv[1].replace('index.js','').replace(/\\/g,'/');;
+var projectPath = path.dirname(fs.realpathSync(__filename));
+var userArgs = process.argv.slice(2);
+var devDir = "dev";
 // on init
-if(userArgs.length ===1 ){
-    if(userArgs[0] === "init") {
-        exec('npm install git://github.com/mmilad/jhcr --save');
+if(userArgs.length ===1 ) {
+    if (fs.existsSync(jhcrPath+'commands/'+userArgs[0]+'/run.js')) {
+        var init = require(jhcrPath+'commands/'+userArgs[0]+'/run.js');
+        init.run({
+            jhcrPath: jhcrPath,
+            devDir:devDir
+        });
+    } else {
+        console.log("command not found")
     }
-    fs.mkdir('src', function (err){
-        // if (err) return console.log("already initialzied");
-        fs.mkdir('src/assets');
-        fs.mkdir('src/js');
-        fs.mkdir('src/css');
-    });
-    console.log("initializing")
+    // if(userArgs[0] === "init") {
+    //     var init = require('./commands/'+userArgs[0]+'/run.js');
+    //     init.run({devDir:devDir});
+    // } else if(userArgs[0] === "build") {
+    // 
+} else {
+// create start 
+if((userArgs[0] === "n") || (userArgs[0] === "new")) {
+    if((userArgs[1] === "c") || (userArgs[1] === "component")) {
+        if(userArgs[2]) {
+            fs.mkdir(devDir+'/src/components/'+userArgs[2], function(err) {
+                fs.writeFile(devDir+'/src/components/'+userArgs[2]+'/component.js', `
+J.registry.`+userArgs[2]+` = {
+    onSet = function (elem) {
+        elem.innerHTML = "Hello World!";
+    }
+};
+                `);
+                fs.writeFile(devDir+'/src/components/'+userArgs[2]+'/style.js', `
+J.C({"`+userArgs[2]+`": {}});
+                `);
+            });
+        }
+    }
 }
+
+}
+
+
+
+
 
 // // on read file
 // if(userArgs[0] === "rf") {
@@ -60,3 +92,60 @@ if(userArgs.length ===1 ){
 // function readFile() {
 
 // }
+// var jsonStr = {
+//     tag: "html",
+//     attributes: {
+//         src:"foo",
+//         href: "me"
+//     },
+//     children: [
+//         {
+//             tag: "head",
+//             children: [
+//                 {
+//                     tag: "script",
+//                     attributes: {
+//                         src: "./../node_modules/jhcr/dist/jhcr.js"
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             tag: "body"
+//         }
+//     ]
+// }
+
+// var tl=0;
+// function jsonToHtmlString(config) {
+//     var l,
+//         txt="",
+//         attrList="",
+//         content="",
+//         tabs="";
+//     tabs = JSON.parse(getTabs(tl))
+//     tl++;
+//     txt = tabs+"<"+config.tag;
+//     for(l in config.attributes) {
+//         attrList = attrList +' '+l+'="'+config.attributes[l]+'"'
+//     }
+//     console.log(attrList)
+//     for(l in config.children) {
+//         content = content + jsonToHtmlString(config.children[l])
+//     };
+//     txt = txt + attrList +'>\n'+tabs+content+'\n'+tabs+'</'+config.tag+'>\n';
+//     tl--;
+//     return txt;
+// }
+// var ee = jsonToHtmlString(jsonStr);
+// function getTabs(amount){
+//     var txt="", i;
+//     for(i=0; i<amount; i++) {
+//         txt = txt +"\t";
+//     }
+//     txt = JSON.stringify(txt);
+//     return txt;
+// }
+// var dd = getTabs(3)
+// console.log(ee);
+// console.log(dd);
